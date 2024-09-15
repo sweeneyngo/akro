e43707b97983a27f1c5b0321a9d5a7e263674d0f
Sweeney Ngo <sweeneyngo@proton.me>
2024-09-15 03:59:17 -0700

    Update subdirectory to relative base

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ifuxyl.dev/akro/", // Set the base path for production
+  base: "/akro/", // Set the base path for production
 })
